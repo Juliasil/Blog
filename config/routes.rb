d607110dc8cd7b4e8b8af297f6bbe7f2d68d9fd6
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2023-11-18 23:21:25 -0300

    route settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :blog_posts
+  resources :blog_posts do
+    get :search, on: :collection
+  end
   resources :comments, only: %i[create]
 
   root "blog_posts#index"
