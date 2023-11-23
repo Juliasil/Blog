fbb4f58e4648398b10b11001d240fc10507efd73
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2023-11-22 22:10:14 -0300

    Updates to the  file.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_16_235040) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_22_235756) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_16_235040) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.datetime "published_at"
   end
 
   create_table "comments", force: :cascade do |t|
